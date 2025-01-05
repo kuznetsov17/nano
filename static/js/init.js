8cc4c746150badcf01a133060c1d7774f597d3bc
kuznetsov17 <m@viku.me>
2025-01-05 09:07:34 +0200

    new-start inmitial push. work in progress

---
+++ static/js/init.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    // enableCopy();
+    if (document.head.dataset['buildSearchIndex'] == "true") {
+      initSearch();
+    }
+    setStartTheme();
+  });
